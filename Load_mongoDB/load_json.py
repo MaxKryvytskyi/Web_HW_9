dfb874ebe208817e12de618fc5acdbf8e20bc915
Max <maxkryvytskyiwork@gmail.com>
2024-02-03 01:36:30 +0200

    save

--- Load_mongoDB/load_json.py
+++ Load_mongoDB/load_json.py
@@ -3,22 +3,25 @@ from models import Authors, Quotes
 from connect_db import connect
 
 
+def main():
+    with open("Load_mongoDB/Json/authors.json", "r+", encoding='utf-8') as file:
+        authors_data = json.load(file)
 
-with open("Load_mongoDB/Json/authors.json", "r+", encoding='utf-8') as file:
-    authors_data = json.load(file)
+    with open("Load_mongoDB/Json/quotes.json", "r+", encoding='utf-8') as file:
+        quotes_data = json.load(file)
 
-with open("Load_mongoDB/Json/quotes.json", "r+", encoding='utf-8') as file:
-    quotes_data = json.load(file)
+    for author_data in authors_data:
+        author = Authors(**author_data)
+        author.save()
 
-for author_data in authors_data:
-    author = Authors(**author_data)
-    author.save()
+    for quote_data in quotes_data:
+        author_name = quote_data["author"]
+        author = Authors.objects(fullname=author_name).first()
 
-for quote_data in quotes_data:
-    author_name = quote_data["author"]
-    author = Authors.objects(fullname=author_name).first()
-
-    if author:
-        quote_data['author'] = author
-        quote = Quotes(**quote_data)
-        quote.save()
+        if author:
+            quote_data['author'] = author
+            quote = Quotes(**quote_data)
+            quote.save()
+        
+if __name__ == "__main__":
+    main()
